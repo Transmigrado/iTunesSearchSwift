6439b3d4c324f9eb345ad00a09de4b17482cba50
transmigrado <transmigrado@gmail.com>
2020-01-02 20:47:02 -0300

    add mock for track api

--- iTunesSearchTests/TrackApiTest.swift
+++ iTunesSearchTests/TrackApiTest.swift
@@ -13,19 +13,25 @@ import Swinject
 
 class TrackApiTest: QuickSpec {
     
+    struct mockTrackAPi: Api {
+              
+       func retrieve(urlString: String, handler: @escaping ((Any) -> Void)) {
+           handler("hello world")
+       }
+    }
     
      override func spec() {
         
         var container: Container!
         beforeEach {
             container = Container()
-            container.register(Api.self) { _ in TrackApi() }
+            container.register(Api.self) { _ in mockTrackAPi() }
         }
         
         it("fetch correctly data"){
             let fetcher = container.resolve(Api.self)!
             fetcher.retrieve(urlString: "http://www.google.com") { response in
-                expect(response).to(contain("hello world"))
+                expect(response as? String).to(contain("hello world"))
             }
         }
     }
