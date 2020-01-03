66a25d76faed60ce4465e8bfe32f3efea61e5891
transmigrado <transmigrado@gmail.com>
2020-01-03 12:40:42 -0300

    add searchbar in SearchTrack

--- iTunesSearch/Screen/SearchTrack.swift
+++ iTunesSearch/Screen/SearchTrack.swift
@@ -12,6 +12,30 @@ import Swinject
 struct SearchTrack: View {
      
   var container: Container
+
+    @State var searchText = ""
+    
+    var searchBar : some View {
+     HStack {
+        HStack {
+            Image(systemName: "magnifyingglass")
+            TextField("Buscar", text: self.$searchText)
+                 .foregroundColor(.primary)
+            Button(action: {
+                self.searchText = ""
+            }) {
+                Image(systemName: "xmark.circle.fill")
+                 .opacity(self.searchText == "" ? 0 : 1)
+            }
+        }
+        .padding(EdgeInsets(top: 8, leading: 6, bottom: 8, trailing: 6))
+        .foregroundColor(.secondary)
+        .background(Color(.secondarySystemBackground))
+        .cornerRadius(10.0)
+    }
+   .padding(.horizontal)
+     
+ }
     
   var list: some View {
     let view: TrackList = container.resolve(from: .trackList)
@@ -21,6 +45,7 @@ struct SearchTrack: View {
   var body: some View {
       NavigationView{
           VStack{
+             searchBar   
              list
                 .navigationBarTitle("Buscar")
           }
