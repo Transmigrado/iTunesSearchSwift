424404cd98da428455cbb5e0269a60d0f699752a
transmigrado <transmigrado@gmail.com>
2020-01-03 18:00:44 -0300

    add play button for reproduction

--- iTunesSearch/View/Row.swift
+++ iTunesSearch/View/Row.swift
@@ -13,10 +13,21 @@ struct Row: View {
     
     var track : Track
     
+    var button : some View {
+        ZStack{
+            Circle()
+                .foregroundColor(Color.white)
+                .frame(width: 30, height: 30, alignment: .center)
+                .opacity(0.5)
+            Image(systemName: "play")
+        }
+    }
+    
     var body: some View {
         HStack{
             URLImage(URL(string: track.artworkUrl100)!)
                 .frame(width: 100.0, height: 100.0, alignment: .center)
+                .overlay(button)
             VStack(alignment: .leading, spacing: 0.0){
                 Text(track.trackName)
                     .bold()
